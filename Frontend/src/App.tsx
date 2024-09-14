7abcea4471ebd9ce1d4162470374fbd3b4bad88d
phamthithhuong14 <huongpttph34059@fpt.edu.vn>
2024-09-14 21:22:23 +0700

    register, login

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Register from './pages/client/Register'
+import { ToastContainer } from 'react-toastify'
+import Login from './pages/client/Login'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-center text-3xl'>asd</h1>
+      <ToastContainer />
+      <Routes>
+        <Route path='/register'element= {<Register />}/>
+        <Route path='/login'element= {<Login />}/>
+      </Routes>
+     
     </>
   )
 }
---
+++ Frontend/src/pages/client/Login.tsx
@@ -0,0 +1,66 @@
+import Joi from 'joi';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom'
+import { User } from '../../interfaces/User';
+import instance from '../../Api';
+
+const userSchema = Joi.object({
+  email: Joi.string().required().email({ tlds: false }),
+  password: Joi.string().required().min(6)
+})
+
+const Login = () => {
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<User>()
+
+  const onSubmit = (user: User) => {
+    ;(async () => {
+      const { data } = await instance.post("/login", user)
+      console.log(data)
+      if (data.user) {
+        sessionStorage.setItem('accessToken', data.accessToken)
+        const isConfirm = confirm('Register successfuly')
+        if (isConfirm) {
+          navigate('/')
+        }
+      }
+    })()
+  }
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+        <h1>Login</h1>
+        <div className='form-group'>
+          <label htmlFor='title'>email</label>
+          <input
+            className='form-control'
+            type='email'
+            placeholder='email...'
+            id='title'
+            {...register('email', { required: true })}
+          />
+          {errors.email && <span className='text-danger'>This field is required</span>}
+        </div>
+        <div className='form-group'>
+          <label htmlFor='price'>Password</label>
+          <input
+            className='form-control'
+            type='number'
+            placeholder='password...'
+            id='price'
+            {...register('password', { required: true, minLength: 6 })}
+          />
+          {errors.password && <span className='text-danger'>This field is required</span>}
+        </div>
+
+        <button className='btn btn-primary w-100'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
