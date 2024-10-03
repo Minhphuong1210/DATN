6a0e4874e917b3da35a89a77b9317211d4037b7f
Hungvy24 <hungvydz2004@gmail.com>
2024-10-04 00:24:08 +0700

    payment momo

--- Frontend/src/components/client/checkout/PaymentForm.tsx
+++ Frontend/src/components/client/checkout/PaymentForm.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
 import { Collapse, CollapseProps } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useCarts } from '../../../hook/Cart';
+import { Cart_detail } from '../../../interfaces/Cart';
 
 interface PaymentFormProps {
     paymentMethod: string;
     setPaymentMethod: (value: string) => void;
 }
-
 const PaymentForm: React.FC<PaymentFormProps> = ({ paymentMethod, setPaymentMethod }) => {
     const navigate = useNavigate();
 
@@ -18,9 +20,24 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ paymentMethod, setPaymentMeth
     const handlePaymentMethodChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const selectedMethod = event.target.value;
         setPaymentMethod(selectedMethod);
-        navigate('/payment');
+        // navigate('/payment');
     };
 
+    // const totalAmount = productCart.reduce( (acc, item) => acc + item.PriceProduct * item.quantity, 0);
+
+    const handlePayment = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/payment/momo', {
+                orderInfo: 'Thông tin đơn hàng',
+                amount: 10000, // Số tiền thanh toán
+            });
+    
+            const { payUrl } = response.data; // Giả sử bạn nhận được URL thanh toán từ MoMo
+            window.location.href = payUrl; // Chuyển hướng người dùng đến trang thanh toán MoMo
+        } catch (error) {
+            console.error('Payment error:', error);
+        }
+    };
     const text = "Sau khi nhấp vào “Thanh toán ngay”, bạn sẽ được chuyển hướng đến VNPAY để hoàn tất việc mua hàng một cách an toàn.";
     const items: CollapseProps['items'] = [
         {
@@ -53,6 +70,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ paymentMethod, setPaymentMeth
                     <FormControlLabel value="Momo" control={<Radio />} label="Momo" />
 
                 </RadioGroup>
+                <button onClick={handlePayment} className='bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded'>Momo</button>
             </FormControl>
 
         </div >
